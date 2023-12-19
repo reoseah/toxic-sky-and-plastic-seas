b6793e066604aac3e092bab3c769efc54f61764c
Reoseah <reoseah9@gmail.com>
2023-12-19 01:41:07 +0100

    add garbage block block

--- src/main/java/io/github/reoseah/toxsky/ToxSky.java
+++ src/main/java/io/github/reoseah/toxsky/ToxSky.java
@@ -3,10 +3,7 @@ package io.github.reoseah.toxsky;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.block.Block;
-import net.minecraft.item.AliasedBlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.PlaceableOnWaterItem;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
@@ -17,6 +14,7 @@ public class ToxSky implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("toxsky");
 
     public static final Block FLOATING_GARBAGE = new FloatingGarbageBlock(Block.Settings.create());
+    public static final Block GARBAGE_BLOCK = new Block(Block.Settings.create().strength(2F));
 
     public static final Item PLASTIC_GARBAGE = new PlaceableOnWaterItem(FLOATING_GARBAGE, new Item.Settings()) {
         @Override
@@ -30,13 +28,16 @@ public class ToxSky implements ModInitializer {
         LOGGER.info("Poisoning atmosphere and polluting oceans!");
 
         Registry.register(Registries.BLOCK, "toxsky:floating_garbage", FLOATING_GARBAGE);
+        Registry.register(Registries.BLOCK, "toxsky:garbage_block", GARBAGE_BLOCK);
 
+        Registry.register(Registries.ITEM, "toxsky:garbage_block", new BlockItem(GARBAGE_BLOCK, new Item.Settings()));
         Registry.register(Registries.ITEM, "toxsky:plastic_garbage", PLASTIC_GARBAGE);
 
         ItemGroup group = FabricItemGroup.builder() //
                 .displayName(Text.translatable("itemGroup.toxsky")) //
                 .icon(PLASTIC_GARBAGE::getDefaultStack) //
                 .entries((context, entries) -> {
+                    entries.add(GARBAGE_BLOCK);
                     entries.add(PLASTIC_GARBAGE);
                 }) //
                 .build();
